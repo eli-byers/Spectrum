1a0de71228c2b9f6d60b0f4fefebfd7bd59c6101
Eli Byers <elibyers.dev@gmail.com>
2018-06-22 02:07:37 -0700

    basic ui

---
+++ Spectrum/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Spectrum
+//
+//  Created by Eli Byers on 6/21/18.
+//  Copyright © 2018 Eli Byers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+//    common aspect ratios
+//    4:3
+//    16:9
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
